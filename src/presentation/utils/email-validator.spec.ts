fa864fd6197e05327c9b0f53e0a5b83a113d3658
Adriano Nascimento de Santana <adriano.santana@fastsolucoes.com.br>
2022-01-25 17:30:35 -0300

    refactor: move makesut to a factory method

--- src/presentation/utils/email-validator.spec.ts
+++ src/presentation/utils/email-validator.spec.ts
@@ -1,27 +1,26 @@
 import { EmailValidatorAdapter } from './email-validator'
 import validator from 'validator'
 
-jest.mock('validator', () => ({
-  isEmail(email: string): boolean {
-    return true
-  }
-}))
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('Email Validator', () => {
   test('Should return false if email validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     var isValid = sut.isValid('invalid_email@gmail.com')
     expect(isValid).toBe(false)
   })
 
   test('Should return true if email validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     var isValid = sut.isValid('valid_email@gmail.com')
     expect(isValid).toBe(true)
   })
 
   test('Should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     sut.isValid('any_email@gmail.com')
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@gmail.com')
