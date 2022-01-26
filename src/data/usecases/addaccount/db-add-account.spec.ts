1543845c9e8dea2d8f2909f3041193fa9838602a
AdrianoSantana <ans3@cin.ufpe.br>
2022-01-26 11:21:51 -0300

    refactor: move sut to makeSut

--- src/data/usecases/addaccount/db-add-account.spec.ts
+++ src/data/usecases/addaccount/db-add-account.spec.ts
@@ -2,18 +2,30 @@ import { AddAccountModel } from '../../../domain/usecases/add-account'
 import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
 
+interface SutTypes {
+  sut: DbAddAccount,
+  encrypterStub: Encrypter
+}
+const makeSut = (): SutTypes => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed_value'))
+    }
+  }
+
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return {
+    sut,
+    encrypterStub
+  }
+}
 describe('SignUp Controller', () => {
   test('Should call encrypter with correct password', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt(value: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_value'))
-      }
-    }
-
-    const encryptStub = new EncrypterStub()
-    const sut = new DbAddAccount(encryptStub)
-
-    const encryptSpy = jest.spyOn(encryptStub, 'encrypt')
+    
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData: AddAccountModel = {
       email: 'valid_email@mail.com',
       name: 'valid_name',
