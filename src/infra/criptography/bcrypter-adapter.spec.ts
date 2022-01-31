cb620fd739d3e52a852864f20b482735f0ae15fc
ans3 <ans3@cin.ufpe.br>
2022-01-31 19:49:01 -0300

    refactor: move sut to a helper factory

--- src/infra/criptography/bcrypter-adapter.spec.ts
+++ src/infra/criptography/bcrypter-adapter.spec.ts
@@ -6,18 +6,21 @@ jest.mock('bcrypt', () => ({
         return new Promise(resolve => resolve('hashedValue'))
     }
 }))
+
+const SALT = 12
+const makeSut = () => {
+    return new BcryptAdapter(SALT)
+}
 describe('bcrypt adapter', () => {
     test('Should call bcrypt with correct value', async() => {
-        const SALT = 12
-        const sut = new BcryptAdapter(SALT)
+        const sut = makeSut()
         const hashSpy = jest.spyOn(bcrypt, 'hash')
         await sut.encrypt('any_value')
         expect(hashSpy).toBeCalledWith('any_value', SALT)
     })
 
     test('Should return a hash on success', async() => {
-        const SALT = 12
-        const sut = new BcryptAdapter(SALT)
+        const sut = makeSut()
         const hashedValue = await sut.encrypt('any_value')
         expect(hashedValue).toBe('hashedValue')
     })
