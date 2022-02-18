9f381b2f9604d11425f8f838d4a4d3e25e725a48
ans3 <ans3@cin.ufpe.br>
2022-02-18 06:29:22 -0300

    refactor: move sut creation to a helper method

--- src/infra/db/mongodb/log-repository/log-error-repository.spec.ts
+++ src/infra/db/mongodb/log-repository/log-error-repository.spec.ts
@@ -1,6 +1,11 @@
 import { Collection } from 'mongodb';
+import { LogErrorRepository } from '../../../../data/protocols/log-error-repository';
 import { MongoHelper } from '../helpers/mongo-helper'
-import { LogMongoRepository } from './log-mongo-repository';
+import { LogMongoRepository } from './log-mongo-repository'
+
+const makeSut = (): LogErrorRepository => {
+  return new LogMongoRepository()
+}
 
 describe('Account Mongo Repository', () => {
   let errorCollection: Collection;
@@ -18,7 +23,7 @@ describe('Account Mongo Repository', () => {
   })
 
   test('Should create an error log on success', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
